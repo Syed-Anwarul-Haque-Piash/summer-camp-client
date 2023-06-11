b8c05b9312c08a409ab4a7cdd78bee048ae987d5
Syed-Anwarul-Haque-Piash <syed88783@gmail.com>
2023-06-11 12:53:43 +0600

    feedback and show modal

--- src/pages/AllClasses/AllClasses.jsx
+++ src/pages/AllClasses/AllClasses.jsx
@@ -1,8 +1,12 @@
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
-import React from 'react';
+import React, { useState } from 'react';
 
 const AllClasses = () => {
+    const [feedback, setFeedback] = useState("")
+    const [textAreaValue, setTextAreaValue] = useState("");
+   
+   
 
     const { data: allClassData = [], refetch } = useQuery(
         ["classes"],
@@ -11,7 +15,7 @@ const AllClasses = () => {
             return res.data;
         }
     );
-    console.log(allClassData)
+    //console.log(allClassData)
 
     const approveHandler = (id) => {
         console.log(id)
@@ -35,6 +39,55 @@ const AllClasses = () => {
                 }
             })
     }
+    const onChangeHandler=()=>{
+        //console.log(textAreaValue);
+        const info={feedback:textAreaValue}
+        fetch(`http://localhost:5000/feedback/${feedback}`,{
+            method: 'PUT',
+            headers:{
+                'content-type':'application/json'
+            },
+            body:JSON.stringify(info)
+
+        })
+        .then(res=>res.json())
+            .then(data=>{
+                console.log(data)
+                console.log(info)
+            })
+    }
+    const content = <>
+        {/* <dialog id="my_modal_3" className="modal">
+                <form method="dialog" className="modal-box">
+                    <button htmlFor="my-modal-3" className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                    <h3 className="font-bold text-lg">Feedback</h3>
+                   
+                    <div>
+                       <textarea onClick={(e)=>setTextAreaValue(e.target.value)} className='border w-full' rows="5"></textarea>
+                       <div className='flex justify-between'>
+                        <button  className="btn btn-success">Submit Feedback</button>
+                        <button className="btn btn-error">Denied</button>
+                       </div>
+                    </div>
+                </form>
+            </dialog> */}
+        <div className="modal" id="my_modal_8">
+            <div className="modal-box">
+                <h3 className="font-bold text-lg">Hello!</h3>
+                <div>
+                    <textarea onChange={(e) => setTextAreaValue(e.target.value)} className='border w-full' rows="5"></textarea>
+                    <div className='flex justify-center'>
+                        <button onClick={onChangeHandler} className="btn btn-success">Submit Feedback</button>
+                        
+                    </div>
+                </div>
+                <div className="modal-action">
+                    <a href="#" className="btn">Close</a>
+                </div>
+            </div>
+        </div>
+    </>
+
     return (
         <div className="overflow-x-auto">
             <table className="table table-xs">
@@ -51,22 +104,23 @@ const AllClasses = () => {
                 </thead>
                 <tbody>
                     {/* {allClassData.map(singleData=><p>1</p>)} */}
-                    {allClassData.map((singleData,index) =>
+                    {allClassData.map((singleData, index) =>
                         <tr key={singleData._id}>
-                            <th>{index+1}</th>
+                            <th>{index + 1}</th>
                             <td><img className='w-14 h-14 rounded-lg' src={singleData.image}></img></td>
                             <td>{singleData.name}</td>
                             <td>{singleData.price}</td>
                             <td>{singleData.instructor}</td>
                             <td><button onClick={() => approveHandler(singleData._id)} className="btn btn-success">Approved</button></td>
-                            <td><button onClick={()=>denyHandler(singleData._id)} className="btn btn-error">Denied</button></td>
-                            <td><button className="btn btn-error">Feedback</button></td>
+                            <td><button onClick={() => denyHandler(singleData._id)} className="btn btn-error">Denied</button></td>
+                            <td ><button className="btn"><a onClick={() => setFeedback(singleData._id)} href="#my_modal_8" className="btn">Feedback</a></button></td>
                         </tr>)}
 
 
                 </tbody>
 
             </table>
+            {content}
         </div>
     );
 };
--- src/pages/MyClass/MyClass.jsx
+++ src/pages/MyClass/MyClass.jsx
@@ -22,12 +22,24 @@ const MyClass = () => {
                         <figure><img src={data.image} alt="Shoes" /></figure>
                         <div className="card-body">
                             <h2 className="card-title">Total Inrolment: 0</h2>
-                            <p><div className="badge badge-neutral">Pending</div></p>
+                            <p><div className="badge badge-neutral">{data.status}</div></p>
                             {/* <div className="card-actions ">
                                 <button className="btn btn-success">Approved</button>
                                 <button className="btn btn-info">Pending</button>
                                 <button className="btn btn-error">Denied</button>
                             </div> */}
+                            {/* The button to open modal */}
+                            <label htmlFor="my_modal_7" className="btn">Your Feedback</label>
+
+                            {/* Put this part before </body> tag */}
+                            <input type="checkbox" id="my_modal_7" className="modal-toggle" />
+                            <div className="modal">
+                                <div className="modal-box">
+                                    <h3 className="text-lg font-bold">Feedback</h3>
+                                    <p className="py-4">{data?.feedback?.feedback || "feetback not found"}</p>
+                                </div>
+                                <label className="modal-backdrop" htmlFor="my_modal_7">Close</label>
+                            </div>
                         </div>
                     </div>)
             }
