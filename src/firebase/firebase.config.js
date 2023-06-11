aa61567a81c6c7f89a6284b6fc111e7fb59ba7b4
Syed-Anwarul-Haque-Piash <syed88783@gmail.com>
2023-06-11 13:20:05 +0600

    hide firebase config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDOfyjt2rlzTpQQS5_jxVZgpm11gK7x1NA",
-  authDomain: "summer-camp-cfbaa.firebaseapp.com",
-  projectId: "summer-camp-cfbaa",
-  storageBucket: "summer-camp-cfbaa.appspot.com",
-  messagingSenderId: "183953643289",
-  appId: "1:183953643289:web:8517c04dfd8a8a4d365788"
+  apiKey:import.meta.env.VITE_apiKey ,
+  authDomain:import.meta.env.VITE_authDomain ,
+  projectId:import.meta.env.VITE_projectId ,
+  storageBucket:import.meta.env.VITE_storageBucket ,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId ,
+  appId:import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
